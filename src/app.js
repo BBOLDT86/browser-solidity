fa5b7fa09b46422509cb86dafdaff6a1ab3db2e3
Alex Beregszaszi <alex@rtfs.hu>
2017-01-09 03:19:21 +0000

    Merge fileTabTemplate

--- src/app.js
+++ src/app.js
@@ -250,7 +250,7 @@ var run = function () {
     $('#output').empty()
 
     for (var f in files) {
-      $filesEl.append(fileTabTemplate(files[f]))
+      $filesEl.append($('<li class="file"><span class="name">' + files[f] + '</span><span class="remove"><i class="fa fa-close"></i></span></li>'))
     }
 
     if (editor.cacheFileIsPresent()) {
@@ -261,10 +261,6 @@ var run = function () {
     $('#input').toggle(editor.cacheFileIsPresent())
     $('#output').toggle(editor.cacheFileIsPresent())
     reAdjust()
-  }
-
-  function fileTabTemplate (name) {
-    return $('<li class="file"><span class="name">' + name + '</span><span class="remove"><i class="fa fa-close"></i></span></li>')
   }
 
   var $filesWrapper = $('.files-wrapper')
